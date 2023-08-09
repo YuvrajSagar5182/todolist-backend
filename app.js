ef1c4dccccf1ee4e5b977b3a97921a377ddbded9
Yuvraj Sagar <yuvrajsagar117@gmail.com>
2023-08-10 00:14:57 +0500

    Adding vercel.json

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const PORT = 8080
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+
 
 const connetDB = require('./config/db')
 
@@ -9,11 +9,16 @@ const todoRoutes = require('./routes/todos')
 const app = express();
 app.use(cors());
 app.use(express.json());
+require('dotenv').config();
 
 
 connetDB();
 
 app.use('/todos', todoRoutes)
+app.get('/', (req, res) => {
+    res.send("<h1>Hello from Yuvi's Todo's Backend</h1>")
+
+})
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
--- config/db.js
+++ config/db.js
@@ -7,7 +7,9 @@ const connectDB = async () => {
   try {
     // await mongoose.connect("mongodb://yuvrajsagar117:learnmongoDB@ac-qjiv1u5-shard-00-00.duf0crf.mongodb.net:27017,ac-qjiv1u5-shard-00-01.duf0crf.mongodb.net:27017,ac-qjiv1u5-shard-00-02.duf0crf.mongodb.net:27017/?ssl=true&replicaSet=atlas-lookq6-shard-0&authSource=admin&retryWrites=true&w=majority",
 
-    await mongoose.connect(`mongodb+srv://${username}:${password}@clusteryuvi.duf0crf.mongodb.net/?retryWrites=true&w=majority`,
+    // await mongoose.connect(`mongodb+srv://yuvrajsagar117:learnmongoDB@clusteryuvi.duf0crf.mongodb.net/?retryWrites=true&w=majority`,
+    await mongoose.connect("mongodb://yuvrajsagar117:learnmongoDB@ac-qjiv1u5-shard-00-00.duf0crf.mongodb.net:27017,ac-qjiv1u5-shard-00-01.duf0crf.mongodb.net:27017,ac-qjiv1u5-shard-00-02.duf0crf.mongodb.net:27017/?ssl=true&replicaSet=atlas-lookq6-shard-0&authSource=admin&retryWrites=true&w=majority",
+
       {
         useNewUrlParser: true,
         useUnifiedTopology: true
